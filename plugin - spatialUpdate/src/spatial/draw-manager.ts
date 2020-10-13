19542236a4ad8f8e68dd71b8a2c5a4ad33f8febb
zroberts1996 <66685795+zroberts1996@users.noreply.github.com>
2020-10-13 18:11:28 -0400

    Update draw-manager.ts

--- plugin - spatialUpdate/src/spatial/draw-manager.ts
+++ plugin - spatialUpdate/src/spatial/draw-manager.ts
@@ -139,6 +139,18 @@ export class DrawManager {
                         that.setActive(this.controls, 'edit');
                     }
                 },
+                help: {
+                    name: 'help',
+                    label: 'plugins.clssPlugin.draw.help',
+                    icon:  "M19 5v14H5V5h14m1.1-2H3.9c-.5 0-.9.4-.9.9v16.2c0 .4.4.9.9.9h16.2c.4 0 .9-.5.9-.9V3.9c0-.5-.5-.9-.9-.9zM11 7h6v2h-6V7zm0 4h6v2h-6v-2zm0 4h6v2h-6zM7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7z",
+                    tooltip: 'plugins.clssPlugin.draw.help',
+                    active: false,
+                    createIcon: () => { that.createIcon(this.controls.help) },
+                    selected: () => this.controls.help.active,
+                    action: () => {
+                        window.open("https://www.youtube.com/");
+                    }
+                },
                 measure: {
                     name: 'measure',
                     label: 'plugins.clssPlugin.draw.measure',
