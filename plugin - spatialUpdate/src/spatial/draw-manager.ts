c62d256413aed0ed11dcd48f1d63450d965039b2
zroberts1996 <66685795+zroberts1996@users.noreply.github.com>
2020-10-14 09:13:09 -0400

    Update draw-manager.ts

--- plugin - spatialUpdate/src/spatial/draw-manager.ts
+++ plugin - spatialUpdate/src/spatial/draw-manager.ts
@@ -148,7 +148,7 @@ export class DrawManager {
                     createIcon: () => { that.createIcon(this.controls.help) },
                     selected: () => this.controls.help.active,
                     action: () => {
-                        window.open("https://keen-bohr-eceea7.netlify.app");
+                        window.open("https://admiring-nobel-9a6de4.netlify.app");
                     }
                 },
                 measure: {
